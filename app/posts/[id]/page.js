70d313c4d4d1c6293e0bdf33d3c93b9f01ae8bf7
Biplab Paul <biplab@codersbucket.com>
2025-05-05 16:32:43 +0600

    post with comment

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import getPost from '../../../lib/getPost';
+import getPostComments from '../../../lib/getPostComment';
+import Comments from '../../component/comments';
+
+export async function generateMetadata({ params }) {
+    const { id } = params;
+    const post = await getPost(id);
+
+    return {
+        title: post.title,
+        description: post.body,
+    }
+}
+
+
 
 export default async function PostPage({ params }) {
     const { id } = params;
-    const post = await getPost(id);
+    const postPromise = getPost(id);
+    const commentPromise = getPostComments(id);
+
+    //const [post, comments] = await Promise.all([postPromise, commentPromise]);
+
+    const post = await postPromise;
     return (
         <div className='mt-6'>
             <h2>{post.title}</h2>
             <p>
                 {post.body}
             </p>
+            <hr></hr>
+            <Suspense fallback={<h1>Loading comments ...</h1>}>
+                <Comments promise={commentPromise} />
+            </Suspense>
         </div>
     )
 }
